be652c1195a8af56e41a50b5b8c84df3bb2a8129
ÖMER MUTLU <omermutlu29@gmail.com>
2024-08-04 05:27:51 +0300

    update recording events

--- background.js
+++ background.js
@@ -1,27 +1,14 @@
-chrome.webNavigation.onCommitted.addListener(function(details) {
-    console.log(details, "commited");
-    if (details.frameId === 0) {
-        chrome.storage.local.get(['events'], function(result) {
-            let events = result.events || [];
-            events.push({
-                url: details.url,
-                timestamp: new Date().toISOString(),
-                type: 'navigation'
-            });
-            chrome.storage.local.set({ events: events });
-
-        })
-    }
-});
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     console.log(details,"onHistoryStateUpdated");
     if (details.frameId === 0) {
         chrome.storage.local.get(['events'], function(result) {
+        
             let events = result.events || [];
             events.push({
                 url: details.url,
                 timestamp: new Date().toISOString(),
+                reason:details.transitionType,
                 type: 'navigation'
             });
             chrome.storage.local.set({ events: events });
